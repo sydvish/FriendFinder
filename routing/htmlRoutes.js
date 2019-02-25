13695cedcea65cdd2d87c370be16b8a002440c4a
sydney Volk <sydneyvolk@sydneys-MacBook-Pro.local>
2019-02-25 14:42:57 -0700

    commit

--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -2,10 +2,10 @@ var path = require("path");
 
 module.exports = function (app) {
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "app/public/survey.html"));
+        res.sendFile(path.join(__dirname, "../app/public/survey.html"));
     });
     app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "app/public/home.html"));
+        res.sendFile(path.join(__dirname, "../app/public/home.html"));
     });
 }
 
